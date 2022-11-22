1f1926e826b47bee8765d5cb9d65eaea6c1656d4
Felipe Barreto <felipe.barreto@osf.digital>
2022-11-22 00:07:01 -0300

    host application

--- next.config.js
+++ next.config.js
@@ -3,4 +3,24 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
+
+module.exports = {
+  ...nextConfig,
+  webpack(config, options) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "host",
+        remotes: {
+          profile: `profile@${process.env.PROFILE_URL}/_next/static/${
+            isServer ? "ssr" : "chunks"
+          }/remoteEntry.js`,
+        },
+        filename: "static/chunks/remoteEntry.js",
+      })
+    );
+
+    return config;
+  },
+};
